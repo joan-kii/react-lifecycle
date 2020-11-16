7ebf400340938c93b64e55f7bfcfde59d1106011
joan-kii <joan.sb81@gmail.com>
2020-11-16 18:58:21 +0100

    añade lifecycle methods. bug componentDidCatch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
+import React, { Component } from 'react';
 import Counter from './components/Counter';
 
-function App() {
-  return (
-    <div className="App">
-      <Counter />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      mounted: true,
+      ignoreProp: 0,
+      seed: 40
+    }
+
+    this.mountCounter = () => this.setState({mounted: true});
+    this.unmountCounter = () => this.setState({mounted: false});
+    this.ignoreProp = () => this.setState({ignoreProp: Math.random()});
+    this.seedGenerator = () => this.setState({seed: Number.parseInt(Math.random() * 100)});
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.mountCounter} disabled={this.state.mounted}>Activar Contador</button>
+        <button onClick={this.unmountCounter} disabled={!this.state.mounted}>Desctivar Contador</button>
+        <button onClick={this.ignoreProp}>Ignorar</button>
+        <button onClick={this.seedGenerator}>Aleatorio</button>
+        {this.state.mounted ? 
+        <Counter 
+          ignoreProp={this.state.ignoreProp}
+          seed={this.state.seed} /> : 
+          null}
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,21 +1,79 @@
 import React, { Component } from 'react';
 
+const ErrorComponent = () => <div>{props.ignore}</div>;
+
 class Counter extends Component {
   constructor(props) {
+    console.log('Constructor ---------- !!!')
     super(props)
 
     this.state = {
-      counter: 0
+      counter: 0,
+      seed: 0
     }
+
+    this.increment = () => this.setState({counter: this.state.counter + 1});
+    this.decrement = () => this.setState({counter: this.state.counter - 1});
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    if (props.seed && state.seed !== props.seed) {
+      return {
+        seed: props.seed,
+        counter: props.seed
+      }
+    }
+  
+    return null;
+  }
+
+  componentDidMount() {
+    console.log('Component Did Mount ----- !!!');
+    console.log('------------------');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextProps.ignoreProp &&
+      this.props.ignoreProp !== nextProps.ignoreProp) {
+        console.log('Component Should Update --NO RENDER-- !!!')
+        console.log('------------------');
+        return false;
+      }
+    
+    console.log('Component Should Update --RENDER-- !!!')
+    console.log('------------------');
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('Get Snapshot Befor Update ------ !!!')
+    return null;
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('Component Did Update --------- !!!');
+    console.log('------------------');
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.log('Component Did Catch --------- !!!');
+  }
+
+  componentWillUnmount() {
+    console.log('Component Will Unmount -------- !!!');
+    console.log('------------------');
   }
     
   render() {
-    console.log('Render')
+    console.log('Render ---------- !!!');
     return (
       <div>
+        <button onClick={this.increment}>Incrementar</button>
+        <button onClick={this.decrement}>Decrementar</button>
         <div className='counter'>
-            Counter: {this.state.counter}
+            Contador: {this.state.counter}
         </div>
+        <ErrorComponent />
       </div>
     )
   }
